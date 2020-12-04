f5a92c0765ed343f911c4301253df6221d3da2f9
Agnes Chueng <agneschueng@gmail.com>
2020-12-03 23:41:25 -0600

    add states and functions to handle inputs from form & pass into api call

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,13 +1,32 @@
-import React from "react";
+import React, {useState} from "react";
 import Layout from "../components/Layout";
 import BookSearch from "../components/BookSearch";
 import SearchResults from "../components/SearchResults";
+import API from "../utils/API";
 
 function Search() {
+    const [formState, setFormState] = useState({});
+    const [booksState, setBooksState] = useState([]);
+
+    function handleChange(e) {
+        const { name, value } = e.target;
+        setFormState({...formState, [name]: value});
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        API.searchBooks(formState)
+        .then(res => setBooksState(res.data))
+        .catch(err => console.log(err));
+    }
+
     return (
         <Layout>
-            <BookSearch/>
-            <SearchResults/>
+            <BookSearch
+            handleChange={handleChange}
+            handleSubmit={handleSubmit}
+            />
+            <SearchResults searchedBooks={booksState}/>
         </Layout>
     )
 }
