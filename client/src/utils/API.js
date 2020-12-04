03512c9d2c157db393ee5af0832cb8eaa701eadb
Agnes Chueng <agneschueng@gmail.com>
2020-12-03 22:20:47 -0600

    define api methods to be used

---
+++ client/src/utils/API.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+const API = {
+    // get all saved books from database
+    getBooks: () => {
+        return axios.get("/api/books");
+    },
+    // search for book from user input
+    searchBooks: (inputTitle) => {
+        return axios.post("/search", {title: inputTitle});
+    },
+    // save the book entry to database
+    saveBook: (bookData) => {
+        return axios.post("/api/books", bookData);
+    },
+    // remove the book from saved books
+    removeBook: (id) => {
+        return axios.delete("/api/books/"+id);
+    }
+};
+
+export default API;
