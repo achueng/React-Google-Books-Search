827f7af6791370e83e3c741c125c95503f796040
Agnes Chueng <agneschueng@gmail.com>
2020-12-03 23:39:37 -0600

    render list items using data from props

--- client/src/components/SearchResults.js
+++ client/src/components/SearchResults.js
@@ -1,16 +1,29 @@
 import React from "react";
 import { Container, Jumbotron, ListGroup } from "react-bootstrap";
 
-function SearchResults() {
+function SearchResults(props) {
+    const data = props.searchedBooks;
+    // (!data[0]) ? (console.log("nothing")) : (console.log("something"));
     return (
         <Container fluid className="px-4">
             <Jumbotron>
                 <h3 className="px-4 mb-2">Book Search Results</h3>
                 <h6 className="px-4 mb-2 text-muted">Click on the entry to save the book.</h6>
                 <ListGroup className="mx-4">
-                    <ListGroup.Item action variant="light">
-                        Test Book Entry
-                    </ListGroup.Item>
+                    {(!data[0]) ? (
+                        <ListGroup.Item action variant="light">
+                            Search for a book to see entries
+                        </ListGroup.Item>
+                    ) : 
+                    (data.map(entry => {
+                        return (
+                            <ListGroup.Item action variant="light">
+                                Title: {entry.title}
+                                Author: {entry.author}
+                                Description: {entry.description}
+                            </ListGroup.Item>
+                        )
+                    }))}
                 </ListGroup>
             </Jumbotron>
         </Container>
