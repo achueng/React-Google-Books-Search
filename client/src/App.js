33c25d7211fac56705905bcbdf48c68a15e9b42c
Agnes Chueng <agneschueng@gmail.com>
2020-12-03 21:45:29 -0600

    update website layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import NavBar from "./components/NavBar";
 import Search from "./pages/Search";
 import Saved from "./pages/Saved";
 
@@ -8,7 +7,6 @@ function App () {
   return (
     <Router>
       <div className="App">
-        <NavBar />
         <Route exact path="/" component={Search} />
         <Route exact path="/saved" component={Saved} />
       </div>
