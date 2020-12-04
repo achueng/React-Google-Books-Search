9b251192b2aa47bdd01f1b573091023784dc7b3c
Agnes Chueng <agneschueng@gmail.com>
2020-12-04 01:07:42 -0600

    fix event.preventDefault and modify res data to look prettier for prop drilling

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -8,16 +8,21 @@ function Search() {
     const [formState, setFormState] = useState({});
     const [booksState, setBooksState] = useState([]);
 
-    function handleChange(e) {
-        const { name, value } = e.target;
+    function handleChange(event) {
+        const { name, value } = event.target;
         setFormState({...formState, [name]: value});
     }
 
-    function handleSubmit(e) {
-        e.preventDefault();
-        API.searchBooks(formState)
-        .then(res => setBooksState(res.data))
-        .catch(err => console.log(err));
+    function handleSubmit(event) {
+        event.preventDefault();
+        API.searchBook(formState)
+        .then(res => {
+            const searchedBooks = res.data.map(entry => {
+                entry.authors = entry.authors.join(', ');
+                return entry;
+            });
+            setBooksState(searchedBooks)
+        }).catch(err => console.log(err));
     }
 
     return (
