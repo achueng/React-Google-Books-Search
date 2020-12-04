26db7fa6985af915a0b71896d4e10dce255a24ee
Agnes Chueng <agneschueng@gmail.com>
2020-12-03 23:53:45 -0600

    add state and function to get saved books

--- client/src/pages/Saved.js
+++ client/src/pages/Saved.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
 import Layout from "../components/Layout";
 import SavedBooks from "../components/SavedBooks";
+import API from "../utils/API";
 
 function Saved() {
+    const [savedBooks, setSavedBooks] = useState([]);
+
+    function getSavedBooks() {
+        API.getBooks().then(response => {
+            // console.log(response);
+            setSavedBooks(response);
+        })
+    }
+    getSavedBooks();
+    
     return (
         <Layout>
-            <SavedBooks/>
+            <SavedBooks savedBooks={savedBooks}/>
         </Layout>
     )
 }
